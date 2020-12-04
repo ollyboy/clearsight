8d40ee3050e2118e7448a2accb3265025fcff029
ollyboy <mcd.steven@gmail.com>
2020-12-04 22:20:52 +0000

    more stuff

---
+++ clearsight/model/doctype/initiative_or_project/initiative_or_project_dashboard.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'heatmap': False,
+		'heatmap_message': _(''),
+		'fieldname': 'initiative_title',
+		'transactions': [
+			{
+				'label': _('Details'),
+				'items': ['Feature or Program Increment or Phase']
+			}
+		]
+	}
