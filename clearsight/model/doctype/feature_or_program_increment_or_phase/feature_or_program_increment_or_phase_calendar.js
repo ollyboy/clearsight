368ef1fa9a28cc07e36ef4fd33f7c9733a241d18
ollyboy <mcd.steven@gmail.com>
2020-12-04 21:12:12 +0000

    more

--- clearsight/model/doctype/feature_or_program_increment_or_phase/feature_or_program_increment_or_phase_calendar.js
+++ clearsight/model/doctype/feature_or_program_increment_or_phase/feature_or_program_increment_or_phase_calendar.js
@@ -1,16 +1,18 @@
 frappe.views.calendar["Feature or Program Increment or Phase"] = {
+        fields:  ["feature_start_date", "feature_end_date", "feature_status", "feature_title", "latest_percent_done_reported", "initiative_brief", "name"],
         field_map: {
                 "start": "feature_start_date",
                 "end": "feature_end_date",
                 "id": "name",
                 "title": "feature_title",
                 "allDay": 1,
-                "progress": "latest_percent_done_reported"
+                "progress": "latest_percent_done_reported",
+                "status" : "feature_status"
         },
         gantt: true,
         filters: [
                 {
-                        "fieldtype": "Link",
+                        "fieldtype": "data",
                         "fieldname": "initiative_brief",
                         "options": "Project",
                         "label": __("Project")
