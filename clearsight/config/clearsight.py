9eec535f87deb5392e4b5ec2709ef058477c5b20
ollyboy <mcd.steven@gmail.com>
2020-12-05 19:39:11 +0000

    main menu

--- clearsight/config/clearsight.py
+++ clearsight/config/clearsight.py
@@ -4,59 +4,107 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"label": _("Model"),
+			"label": _("Setup"),
 			"icon": "fa fa-star",
 			"items": [
 				{
 					"type": "doctype",
-					"name": "Project",
-					"description": _("Load projects here"),
+					"name": "Company Structure",
+					"description": _(""),
 					"onboard": 1,
 				},
 				{
 					"type": "doctype",
-					"name": "Company Structure",
-					"description": _("Add companies and groups here"),
+					"name": "Resource Pool",
+					"description": _(""),
 					"onboard": 1,
 				},
-                                                            {
-                                        "type": "doctype",
-                                        "name": "Program",
-                                        "description": _("Define programs"),
-                                        "onboard": 1,
-                                },
-                                                            {
-                                        "type": "doctype",
-                                        "name": "Budget",
-                                        "description": _("Define budget"),
-                                        "onboard": 1,
-                                },
-                                                            
-                                                            {
-                                        "type": "doctype",
-                                        "name": "Strategic Intent",
-                                        "description": _("Define Strategic Intent"),
-                                        "onboard": 1,
-                                },
-                                                            {
-                                        "type": "doctype",
-                                        "name": "Goal or Objective",
-                                        "description": _("Define Goals or Objectives"),
-                                        "onboard": 1,
-                                }
-			]
-                },
-
                 {
-                        "label": _("Summary"),
-                        "icon": "fa fa-star",
-                        "items": [
+                    "type": "doctype",
+                    "name": "Key Result Area",
+                    "description": _(""),
+                    "onboard": 1,
+                }
+       
+			]
+        },
+
+        {
+            "label": _("Intention"),
+            "icon": "fa fa-star",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Period or Planning Horizon",
+                    "description": _(""),
+                    "onboard": 1,
+                },
                                 {
-                                        "type": "doctype",
-                                        "name": "Finance Resource forcast",
-                                        "description": _("Generated summary"),
-                                        "onboard": 1,
-                                }
-                        ]
-		}
+                    "type": "doctype",
+                    "name": "Strategic Intent",
+                    "description": _(""),
+                    "onboard": 1,
+                },
+                                {
+                    "type": "doctype",
+                    "name": "Goal or Objective",
+                    "description": _(""),
+                    "onboard": 1,
+                },
+                                {
+                    "type": "doctype",
+                    "name": "Budget",
+                    "description": _(""),
+                    "onboard": 1,
+                }
+            ]
+		},
+            {
+            "label": _("Portfolio"),
+            "icon": "fa fa-star",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Theme or Headline Program",
+                    "description": _(""),
+                    "onboard": 1,
+                },
+                                {
+                    "type": "doctype",
+                    "name": "Initiative or Project",
+                    "description": _(""),
+                    "onboard": 1,
+                },
+                                {
+                    "type": "doctype",
+                    "name": "Feature or Program Increment or Phase",
+                    "description": _(""),
+                    "onboard": 1,
+                },
+                                {
+                    "type": "doctype",
+                    "name": "Capability",
+                    "description": _(""),
+                    "onboard": 1,
+                }
+            ]
+        },
+            {
+            "label": _("Progress"),
+            "icon": "fa fa-star",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Releases and Progress reports",
+                    "description": _(""),
+                    "onboard": 1,
+                },
+                                {
+                    "type": "doctype",
+                    "name": "Risk",
+                    "description": _(""),
+                    "onboard": 1,
+                }
+            ]
+        }
 	]
