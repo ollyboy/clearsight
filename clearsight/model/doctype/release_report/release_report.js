29d795ccb67a7a99760f1436c2bebea4f6d334be
Steven McDonald <mcd.steven@gmail.com>
2022-04-13 10:44:50 +1000

    reports

---
+++ clearsight/model/doctype/release_report/release_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, steve mcd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Release Report', {
+	// refresh: function(frm) {
+
+	// }
+});
